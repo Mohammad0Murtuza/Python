4e7ce10738cc009b2fa2b6cd06def79031d0f3be
Captain_Murtuza <69781075+Mohammad0Murtuza@users.noreply.github.com>
2022-08-02 21:31:58 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,17 @@
+"""
+Created on Thu Jul 23 12:33:14 2020
+
+@author: jari
+"""
+
+def palindrome():
+    print("enter the word")
+    word = input()
+    word.split()
+    word1 = word[::-1]
+    word1
+    if word1 == word:
+        print("palindrome")
+    else :
+        print("matlab kuch bhi thodi to maryada rakhiye")
+palindrome() 
